2fdbc035f695741f6bb5bc4f437814c3982faa3d
AhmedDR200 <alshwwhy212@gmail.com>
2023-07-15 23:30:25 +0300

    5th

--- post/urls.py
+++ post/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('<int:id>', views.post_datail , name='post_datail'),
     path('<int:id>/edit', views.post_edit , name='post_edit'),
     path('create', views.post_create , name='post_create'),
+
+    path('cbv', views.PostList.as_view() , name='post_List_cbv'),
+    path('cbv/create', views.PostCreate.as_view() , name='post_create_cbv'),
+    path('cbv/<int:pk>', views.PostDetail.as_view() , name='post_detail_cbv'),
+    path('cbv/<int:pk>/edit', views.PostUpdate.as_view() , name='post_update_cbv'),
+    
+
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Post
 from .forms import Postform
+from django.views.generic import ListView , DetailView
+from django.views.generic import CreateView , DeleteView , UpdateView
+
 # Create your views here.
 
 
@@ -33,10 +36,34 @@ def post_create(request):
 def post_edit(request , id):
     post = Post.objects.get(id=id)
     if request.method =="POST":
-     form = Postform(request.POST , instance=post)
+     form = Postform( request.POST , instance=post)
      if form.is_valid():
          form.save()
 
     else:
          form = Postform(instance=post)
     return render(request ,'post/post_edit.html' , {'form':form} )
+
+
+
+class PostList(ListView):
+    model = Post
+
+
+class PostDetail(DetailView):
+   model = Post
+
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title','contant','created_at']
+    template_name ='post/post_edit.html'
+    success_url='/blog/cbv'
+
+
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title','contant','created_at','email','views_count']
+    template_name ='post/post_create.html'
+    success_url='/blog/cbv'
